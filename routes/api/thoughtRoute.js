43e8e2dc48d66c98b3586d496872892b661dbf99
David Healy <drtjprdave@gmail.com>
2022-09-27 17:42:41 -0600

    User Coontroller done.

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,13 @@
+// TODO: get all thoughts
+
+// TODO: get one thought
+
+// TODO: post new thought
+
+// TODO: put/update thought
+
+// TODO: delete thought
+
+// TODO: post to create a reaction in a single thought reaction array
+
+// TODO: delete to a reaction...
---
+++ routes/api/userRoute.js
@@ -0,0 +1,13 @@
+// TODO: get all user
+
+// TODO: get one user
+
+// TODO: post new user
+
+// TODO: put/update user
+
+// TODO: delete user
+
+// TODO: post to add a new friend to a user's list
+
+// TODO: delete to remove friend from user's list.
